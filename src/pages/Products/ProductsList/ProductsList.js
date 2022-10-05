d870f72aca48b1a2c8fe8071f351ffcff76a26d5
MarcosSalinas77 <marcos.cortazar@cencosud.com>
2022-10-05 19:14:30 -0300

    cargando listado de productos en productsList

--- src/pages/Products/ProductsList/ProductsList.js
+++ src/pages/Products/ProductsList/ProductsList.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { getProducts} from '../../../services/apiServices'
 
 const ProductsList = () => {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts()
+      .then(data => {
+        setProducts(data)
+      });
+  }, [])
+  
+
   return (
-    <div>ProductsList</div>
+    <div>
+      {products.length ? products.map(p => <h4>{p.title}</h4>)
+      : <h1>cargando...</h1>  
+    }
+    </div>
   )
 }
 
