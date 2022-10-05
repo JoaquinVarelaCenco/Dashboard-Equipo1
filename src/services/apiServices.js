0f59804322c736f46aa7c0ca8b76af2aacc7414e
JoacoVarela <joaquin.varela@cencosud.com>
2022-10-05 17:49:22 -0300

    Peticiones

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -1,9 +1,24 @@
-import React from 'react'
+const url = "http://localhost:5000/api/";
+const getProducts = () => {
+  return fetch(`${url}product`).then((res) => res.json());
+};
 
-const apiServices = () => {
-  return (
-    <div>apiServices</div>
-  )
-}
+const getProductById = (id) => {
+  return fetch(`${url}product/${id}`).then((res) => res.json());
+};
 
-export default apiServices
+const deleteProduct = (productId) => {
+  return fetch(`${url}product?id=${productId}`, {
+    method: "DELETE",
+  }).then((res) => res.json());
+};
+
+const updateProduct = (body) => {
+  return fetch(`${url}product`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: body,
+  }).then((res) => res.json());
+};
+
+module.exports = { getProducts, getProductById, deleteProduct, updateProduct };
