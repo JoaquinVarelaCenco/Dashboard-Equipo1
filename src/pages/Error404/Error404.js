d018fbc7ec98b40df6b33d177ac7b92154624d2c
Pablo Martin Clemente <martin.clemente@cencosud.com>
2022-10-05 21:21:50 -0300

    Creada pagina de error404

--- src/pages/Error404/Error404.js
+++ src/pages/Error404/Error404.js
@@ -1,8 +1,22 @@
+import { Link } from 'react-router-dom';
 import './Error404.css';
 
 const Error404 = () => {
   return (
-    <div className='error'>Error404</div>
+    <>
+        <div className='error404__Main'>
+            <div className='error404__top'>
+                <p>404</p>
+            </div>
+            <div className='error404__bottom'>
+                <h3>Disculpa, Página No Encontrada</h3>
+                <p>La página que deseas acceder no existe</p>
+                <Link to="/">
+                    <button>Home</button>
+                </Link>
+            </div>
+        </div>
+    </>
   )
 }
 
