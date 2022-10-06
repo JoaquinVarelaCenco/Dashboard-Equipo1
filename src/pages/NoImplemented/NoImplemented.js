fb6890c68a134f1f6560287e4c3809a35bcd56b9
Pablo Martin Clemente <martin.clemente@cencosud.com>
2022-10-05 23:09:55 -0300

    Creada pagina de noImplementada

--- src/pages/NoImplemented/NoImplemented.js
+++ src/pages/NoImplemented/NoImplemented.js
@@ -1,8 +1,19 @@
+import { Link } from 'react-router-dom'
 import './NoImplemented.css'
 
 const NoImplemented = () => {
   return (
-    <div>NoImplemented</div>
+    <div className='noImplemented'>
+        <h3><span>En</span> Construcción</h3>
+        <p>Esta sección estara lista pronto</p>
+        <div className='progress'>
+            <div className='progress-bar'>
+            </div>
+        </div>
+        <Link to="/">
+            <button>Home</button>
+        </Link>
+    </div>
   )
 }
 
