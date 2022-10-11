d9177b7fd6671bb987e235d5979f8a3b338a08e7
AguAF <agustin.alonso@cencosud.com>
2022-10-11 13:18:02 -0300

    Updated header - header context

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,86 +1,49 @@
-// import { SearchContext } from "../../context/SearchContext";
-// import searchImage from "../../assets/images/magnify.svg";
-// import leftArrow from '../../assets/images/chevron-right (1).svg'
-import "./Header.css";
-import { useContext, useEffect, useRef, useState } from "react";
-import { HeaderContext } from "../../context/HeaderContext";
-import { useLocation, Link } from "react-router-dom";
-import menuImage from "../../assets/images/menu.svg";
-import { SideBarContext, SideBarProvider } from "../../context/SideBarContext";
 import { ThemeContext } from "../../context/ThemeContext";
-import HeaderProducts from "../HeaderProducts/HeaderProducts";
-import HeaderEditAddProduct from "../HeaderEditAndAddProduct/HeaderEditAddProduct";
-
-
+import "./Header.css"
+import { useContext, useEffect, useRef } from "react";
+import { HeaderContext } from "../../context/HeaderContext";
+import { useLocation } from 'react-router-dom';
+import searchImage from '../../assets/images/magnify.svg';
+import menuImage from '../../assets/images/menu.svg'
 
 const Header = () => {
-  let buttonMenu = useRef();
-
-  //Contextos : tema - page - buscador
-  const { theme } = useContext(ThemeContext);
-  const { page, currentPage, titleContainer, currentTitleContainer } = useContext(HeaderContext);
-
+  const { page, currentPage } = useContext(HeaderContext)
   const location = useLocation();
-  const { toggleVisibility } = useContext(SideBarContext);
-  let pageTitle = "¡Hola Olivia!";
-  let productId = "";
-  let titleLink = "/home";
-
-  //USe effect para setear la url en la que me encuentro
+  let pageTitle ="¡Hola Olivia!";
+  const { theme } = useContext(ThemeContext);
   useEffect(() => {
     currentPage(location.pathname);
-    // titleContainer.current.style.display="flex";
-    currentTitleContainer("displayFlex")
     
   }, [location]);
-
-  if (page === "/products") {
-    pageTitle = "Products";
-    titleLink =page;
-  } else if (page.includes("/stores")) {
-    pageTitle = "Tiendas";
-    titleLink =page;
-  } else if (page === "/products/new") {
-    pageTitle = "Productos";
-    titleLink ="/products";
-  } else if (page.includes("/products/")) {
-    pageTitle = "Productos";
-    titleLink ="/products";
-    productId = "#" + location.pathname.split("/")[2];
+  if(page==='/products'){
+    pageTitle = 'Products';
+  }else if(page === "/stores"){
+    pageTitle = "Tiendas"
   }
+
+  const title = useRef(null);
 
   return (
     <div className={`header ${theme}`}>
-      <div className={`headerGeneric ${titleContainer}`}>
-        <button
-          ref={buttonMenu}
-          onClick={() => {
-            toggleVisibility();
-          }}
-          className="header__menu-hamburguesa"
-        >
-          <img
-            src={menuImage}
-            alt="Menú hamburguesa"
-            className="hamburguerMenu"
-          />
-        </button>
-        <div className="title">
-          <Link to={titleLink}><h2>{pageTitle}</h2></Link>
-        </div>
+      <div className="headerGeneric">
+        <button><img src={menuImage} alt="Menú hamburguesa" className="hamburguerMenu"/></button>
+        <div className="title"><h2 ref={title}>{pageTitle}</h2></div>
       </div>
-
-      {page === "/products" ? (
-        <HeaderProducts/>
-      ) : page === "/products/new" ? (
-        <HeaderEditAddProduct productContent="Nuevo Producto" editProduct={false}/>
-      ) : page.includes("/products/") ? (
-        <HeaderEditAddProduct productContent={productId} editProduct={true}/>
-      ) : (
-        ""
-      )}
+      {
+      page==="/products" ? 
+          <div className="headerProducts">
+            <div className="header__search-container">
+              <input type="text" class="header__search" placeholder="Buscar productos..." />
+              <button><img src={searchImage} alt="Lupa de busqueda" /></button>
+            </div>
+            <button className="headerProducts__btnAgregar">Agregar Productos</button>
+          </div> 
+      :
+      <h1>Chau juan</h1>
+    }
+    
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
--- src/context/HeaderContext.js
+++ src/context/HeaderContext.js
@@ -1,16 +1,22 @@
-import { useState, createContext } from "react";
+import { useState, createContext } from 'react'
 
-export const HeaderContext = createContext("home");
+export const HeaderContext = createContext('home')
 
-export const HeaderProvider = ({ children }) => {
-  const [page, setPage] = useState("home");
+export const HeaderProvider = ({children}) => {
+
+  const [ page, setPage ] = useState('home');
+  const [ titleContainer, setTitleContainer ] = useState('displayFlex')
 
   const value = {
     page,
-    currentPage: (p) => setPage(p),
-  };
+    currentPage:(p) => setPage(p),
+    titleContainer,
+    currentTitleContainer: (p) => setTitleContainer(p)
+  }
 
   return (
-    <HeaderContext.Provider value={value}>{children}</HeaderContext.Provider>
-  );
-};
+    <HeaderContext.Provider value={value}>
+      {children}
+    </HeaderContext.Provider>
+  )
+}
