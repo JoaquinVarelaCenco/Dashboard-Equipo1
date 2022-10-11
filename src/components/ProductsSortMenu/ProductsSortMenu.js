c8df8a333bea7235a3bc2a7eb6b8f524b2cd130c
JoaquinVarelaCenco <111792528+JoaquinVarelaCenco@users.noreply.github.com>
2022-10-11 18:16:58 -0300

    Button folder

--- src/components/ProductsSortMenu/ProductsSortMenu.js
+++ src/components/ProductsSortMenu/ProductsSortMenu.js
@@ -8,19 +8,22 @@ const ProductsSortMenu = () => {
 
   return (
     <div className="productList__container__sort">
+        <div>
         <i>Ordenar por</i>
         <select
-        name="category"
-        required
-        value={context.orderBy}
-        className="select-control"
-        onChange={context.orderProducts}
+          name="category"
+          required
+          value={context.orderBy}
+          className="select-control-products"
+          onChange={context.orderProducts}
         >
-        <option value="Mayor precio">Mayor precio</option>
-        <option value="Menor precio">Menor precio</option>
-        <option value="Mas relevantes" selected>Mas relevantes</option>
-        <option value="A-Z">A-Z</option>
-        </select>   
+          <option value="Mayor precio">Mayor precio</option>
+          <option value="Menor precio">Menor precio</option>
+          <option value="Mas relevantes" selected>Mas relevantes</option>
+          <option value="A-Z">A-Z</option>
+        </select> 
+        </div> 
+        <i>{context.products.length} resultados</i> 
     </div>
   )
 }
