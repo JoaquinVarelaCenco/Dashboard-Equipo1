446f1147bd1ab1ea3ac5ed82c5513aea0a1362e8
MarcosViera77 <111792388+MarcosSalinas77@users.noreply.github.com>
2022-10-18 12:25:05 -0300

    Update ProductsList.test.js

--- src/pages/Products/ProductsList/ProductsList.test.js
+++ src/pages/Products/ProductsList/ProductsList.test.js
@@ -1,27 +1,48 @@
-import { render, screen } from "@testing-library/react";
-import ProductsList from './ProductsList'
-import {BrowserRouter} from 'react-router-dom';
-import productsData from '../../mockData/productsData'
+import { render, act, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import {
+  SearchContext,
+  SearchProvider,
+  handleSearch,
+  products,
+  productsExist,
+  getAllProducts,
+  helpProducts,
+  searchTermValue,
+  orderBy,
+  orderProducts,
+} from "../../../context/SearchContext";
+import productsData from "../../../mockData/productsData";
+import { getProducts } from "../../../services/apiServices";
+import ProductsList from "./ProductsList";
 
+jest.mock("../../../services/apiServices");
 
+describe("Test formulario de productos", () => {
+  let component;
 
-jest.mock('../../../context/SearchContext')
+  beforeEach(async () => {
 
-beforeEach( async()=>{
-    let component;
-    getAllProducts.mockReturnValue(productsData)
-    await act(async ()=> {
-        component = render(<ProductsList/>, { wrapper: BrowserRouter })
-      })
-})
+    getProducts.mockResolvedValue([...productsData])
 
-describe("Test formulario de productos", ()=>{
+    await act(async () => {
+      component = await render(
+      <MemoryRouter><ProductsList /></MemoryRouter>, { wrapper: SearchProvider });
 
-    test("La lista de productos se debe renderizar correctamente", ()=>{})
-    const { container } = component
-    expect(container).toMatchSnapshot()
-    //se renderiza correctamente
-    //muestra cantidad de productos correctamente - baja
-    //se ordena según opción seleccionada - media
+    });
+  });
 
-})
+  it("Se debe renderizar correctamente", async () => {
+    const { container } = component;
+    expect(container).toMatchSnapshot();
+    screen.debug();
+  });
+
+  it("Se debe renderizar correctamente los items", async () => {
+  });
+
+});
+
+//se renderiza correctamente
+//muestra cantidad de productos correctamente - baja
+//se ordena según opción seleccionada - media
