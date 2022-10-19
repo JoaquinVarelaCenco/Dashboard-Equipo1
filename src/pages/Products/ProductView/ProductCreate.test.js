144067fc6482db11ba99f4418511ccceb49bd2d4
MarcosSalinas77 <marcos.cortazar@cencosud.com>
2022-10-19 18:18:35 -0300

    test finales en testProductCard3

--- src/pages/Products/ProductView/ProductCreate.test.js
+++ src/pages/Products/ProductView/ProductCreate.test.js
@@ -1,10 +1,29 @@
 import { render, screen, act } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { ProductProvider } from "../../../context/ProductContext";
+import ProductCreate from "./ProductCreate";
 
 describe("Test formulario de productos", ()=>{
 
-    test("Product Create", ()=>{})
+    let component;
 
-    //validate crear cancelar
+    beforeEach( async()=>{
+        await act(async () => {
+            component = await render(
+                <ProductProvider>
+
+                    <ProductCreate />
+                </ProductProvider>,
+            { wrapper: BrowserRouter });
+    
+        });
+    })
+
+    test("Product create se renderiza correctamente", ()=>{
+        const { container } = component;
+        expect(container).toMatchSnapshot();
+        screen.debug()
+    })
   
 
 
