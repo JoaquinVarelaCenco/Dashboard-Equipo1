824b394d854a7661c4764498c945ec8517d56d01
Pablo Martin Clemente <martin.clemente@cencosud.com>
2022-10-19 17:30:04 -0300

    Intentos de arreglo para las barcharts

--- src/pages/Home/Home.test.js
+++ src/pages/Home/Home.test.js
@@ -5,34 +5,13 @@ import { getProducts } from "../../services/apiServices";
 import mockData from '../../mockData/productsData';
 
 
+
 //mocks
 jest.mock('../../services/apiServices');
 
-// jest.mock("recharts", () => ({ ResponsiveContainer: jest.fn().mockImplementation((
-//       { children }) => children), 
-//       PieChart: jest.fn().mockImplementation(({ children }) => children), 
-//       Pie: jest.fn().mockImplementation(({ children }) => children),
-//       }
-//     ));
-
-// jest.mock('recharts', () => {
-//   const OriginalResponsiveContainerModule = jest.requireActual('recharts');
-
-//   return {
-//     ResponsiveContainer: ({ height, children }) => (
-//       <OriginalResponsiveContainerModule.ResponsiveContainer
-//         width={800}
-//         height={height}
-//       >
-//         {children}
-//       </OriginalResponsiveContainerModule.ResponsiveContainer>
-//     ),
-//   };
-// });
-
-
 let component;
 let products;
+
 
 describe("Tests para la vista Home", () => {
 
