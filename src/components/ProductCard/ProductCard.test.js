e83961a6855e4f4dd52ccb7815052eeedf45a3cc
MarcosSalinas77 <marcos.cortazar@cencosud.com>
2022-10-19 15:22:54 -0300

    testeando link en productCard

--- src/components/ProductCard/ProductCard.test.js
+++ src/components/ProductCard/ProductCard.test.js
@@ -2,6 +2,7 @@ import { render, screen, act } from "@testing-library/react";
 import ProductCard from './ProductCard'
 import {BrowserRouter} from 'react-router-dom'
 import productsData from '../../mockData/productsData'
+import userEvent from "@testing-library/user-event";
 
 
 
@@ -22,19 +23,11 @@ describe("Test Tarjeta de producto", ()=>{
         expect(container).toMatchSnapshot()
     })
 
-    // test("Se oculta spinner cuando la imagen carga", ()=>{
-    //         component = render(<ProductCard
-    //         title={productsData[0].title}
-    //         img = {productsData[0].images[0]}
-    //         id = {productsData[0].id}
-    //     />, 
-    //     { wrapper: BrowserRouter })
-
-
-    //     screen.debug()
-    // })
-
-    // spinner en imagen - baja
+    test("La tarjeta debe ser un link a la vista de update", ()=>{
+        const linkToUpdate = screen.getByRole('link');
+        const expectedLink = '/products/' + productsData[0].id;
+        expect(linkToUpdate.getAttribute('href')).toBe(expectedLink)
+    })  
   
 
 
