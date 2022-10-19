324d158ffc53a5525a7fe3f4a6b7b3d4d74d1202
MarcosSalinas77 <marcos.cortazar@cencosud.com>
2022-10-19 14:22:49 -0300

    testing en app terminado

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,10 @@
-import { queryByTestId, queryByText, render, screen } from '@testing-library/react';
+import { getByText, queryByTestId, queryByText, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 import { HeaderProvider } from "./context/HeaderContext";
 import { SearchProvider } from './context/SearchContext';
 import { SideBarProvider } from "./context/SideBarContext";
-// import ProductsList from './pages/Products/ProductsList/ProductsList'
-// import Header from './components/Header/Header'
-
-// jest.mock('./pages/Products/ProductsList/ProductsList')
-
-
-
 
 let component
 describe("Testing de app", ()=>{
@@ -27,29 +20,32 @@ describe("Testing de app", ()=>{
       )
   })
 
-  // afterEach(()=>{
-  //   window.prompt = jsdomPrompt;
-  // })
   test("Cuando el usuario está en la raíz ('/') se renderiza el componente <Home/>", () => {
     let buttons = screen.queryAllByRole('button', {name: "Ver listado"})
     expect(buttons.length).toBe(2)
   });
   
-  test("Cuando clickeamos en Sidebar-'Productos' se rendirecciona a /products", () => {
+  test("Click en Sidebar-'Productos' rendirecciona a /products y renderiza <ProductsList/>", () => {
     let btnProductos = screen.queryByRole('link', {name: "Productos"})
     userEvent.click(btnProductos);
     const currentRoute = screen.queryByTestId('location-display');
     expect(currentRoute.innerHTML).toBe('/products')
+
+    const verificadorProductsList = screen.queryByText("Mas relevantes");
+    expect(verificadorProductsList).toBeInTheDocument();
   });
 
-  test("Cuando clickeamos en Sidebar-'Tiendas' se rendirecciona a /products", () => {
+  test("Click en Sidebar-'Tiendas' rendirecciona a /stores y renderiza <NoImplemented/>", () => {
     let btnTiendas = screen.queryByRole('link', {name: "Tiendas"})
     userEvent.click(btnTiendas);
     const currentRoute = screen.queryByTestId('location-display');
     expect(currentRoute.innerHTML).toBe('/stores')
+
+    const verificadorComponenteProfile = screen.queryByText("Esta sección estara lista pronto");
+    expect(verificadorComponenteProfile).toBeInTheDocument();
   });
 
-  test("Cuando clickeamos en Sidebar-'Inicio' se rendirecciona a /home", () => {
+  test("Click en Sidebar-'Profile' rendirecciona a /profile y renderiza <NoImplemented/>", () => {
     let btnProfile = screen.queryByRole('link', {name: /Olivia/i})
 
     let currentRoute = screen.queryByTestId('location-display');
@@ -58,14 +54,19 @@ describe("Testing de app", ()=>{
     userEvent.click(btnProfile);
     currentRoute = screen.queryByTestId('location-display');
     expect(currentRoute.innerHTML).toBe('/profile')
+    const verificadorComponenteProfile = screen.queryByText("Esta sección estara lista pronto");
+    expect(verificadorComponenteProfile).toBeInTheDocument();
   });
 
-  test("Cuando clickeamos en Sidebar-'Inicio' se rendirecciona a /home", () => {
+  test("Click en  Sidebar-'Inicio' rendirecciona a /home y renderiza <Home/>", () => {
     let btnsInicio = screen.queryAllByRole('link', {name: "Inicio"});
     let btnInicio = btnsInicio[0];
     userEvent.click(btnInicio);
     const currentRoute = screen.queryByTestId('location-display');
-    expect(currentRoute.innerHTML).toBe('/home')
+    expect(currentRoute.innerHTML).toBe('/home');
+
+    let buttons = screen.queryAllByRole('button', {name: "Ver listado"})
+    expect(buttons.length).toBe(2)
   });
   
 })
